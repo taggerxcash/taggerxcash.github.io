12ad56066abd2ee138320a391aad12441a16ae8a
taggerxcash <blacklist.qwerty@yandex.ru>
2021-12-05 14:27:25 +0700

    First

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+  $('.nav-catalog').on('click', function (e) {
+    $('html,body').stop().animate({ scrollTop: $('.catalog').offset().top }, 1000);
+    e.preventDefault();
+  });
+});
+
+$(function () {
+  $('.nav-about').on('click', function (e) {
+    $('html,body').stop().animate({ scrollTop: $('.footer').offset().top }, 1000);
+    e.preventDefault();
+  });
+});
