f3ad1df223765752155d2c8ef9a2e06b709f7f91
taggerxcash <blacklist.qwerty@yandex.ru>
2021-12-08 23:14:10 +0700

    pre

--- js/main.js
+++ js/main.js
@@ -11,3 +11,16 @@ $(function () {
     e.preventDefault();
   });
 });
+
+
+
+$('.catalog__inner__section__blocks__item').click(function (e) {
+  e.preventDefault();
+  $('.popup__bg-item').fadeIn(300);
+  $('html').addClass('no-scroll');
+});
+
+$('.popup-item-close').click(function () {
+  $('.popup__bg-item').fadeOut(300);
+  $('html').removeClass('no-scroll');
+});
